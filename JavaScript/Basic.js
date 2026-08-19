a8dce15f416f11bfc6ee7d4f1b6cc1b4113995bc
ArbinBhasimaOfficial <arbinbhasimaofficial@gmail.com>
2026-08-19 23:42:29 +0545

    Added JS

---
+++ JavaScript/Basic.js
@@ -0,0 +1,56 @@
+// lesson 1 JS Basics
+
+// must be done in console and copy paste it in here
+
+
+// 1.
+alert('Ohayo Gozaimasu!');
+// displaying 'Ohayo Gozaimasu' in alert box
+
+// 2.
+alert('Arubin Basima');
+// displaying 'Arubin Basima' in alert box
+
+// 3
+10 + 5
+// 15
+// doing simple math
+
+// 4.
+20 - 5
+// 15
+// doing simple math
+
+// 5.
+2 + 2 - 5
+// -1
+// doing simple math
+
+// 6.
+document.body.innerHTML = "Ohayo Gozaimasu";
+// 'Ohayo Gozaimasu'
+// displaying 'Ohayo Gozaimasu' on web page
+
+// 7.
+document.body.innerHTML = "Arbin Bhasima";
+// 'Arbin Bhasima'
+// displaying my name on web page
+
+
+// Challenge Exercise
+
+// 8.
+10 + 8 + 20
+// $38 is to total cost
+// calculate total cost
+
+
+// 9.
+100 - 20 - 50 + 200
+// i have $230
+// calculate movey i have
+
+// 10.
+document.body.innerHTML = "";
+// ''
+// to make web page blank
